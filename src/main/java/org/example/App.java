07b02a093df6edee47bb06cb8aa7c23448d1f99e
BogdanBoboaca <bogdan.m.boboaca@gmail.com>
2021-12-02 15:10:39 +0200

    Studied inheritance

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,37 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the Racing Game!" );
+
+        Car carReference = new Car();
+        carReference.name = "BMW";
+        carReference.color = "Red";
+        carReference.mileage = 9.8;
+        carReference.fuelLevel = 60;
+        carReference.maxSpeed = 200;
+        carReference.running = false;
+        carReference.doorCount= 2;
+
+
+        Car car2 = new Car();
+        car2.name = "Audi";
+        car2.mileage = 14;
+
+        System.out.println("First car name: " + carReference.name);
+        System.out.println("Color - " + carReference.color);
+        System.out.println("Mileage - " + carReference.mileage);
+        System.out.println("Fuel Level - " + carReference.fuelLevel);
+        System.out.println("Max Speed - " + carReference.maxSpeed);
+        System.out.println("Is the car running? - " + carReference.running);
+        System.out.println("Number of doors - " + carReference.doorCount);
+
+
+
+        System.out.println("Second car name: " + car2.name);
+        System.out.println("Mileage - " + car2.mileage);
     }
 }
---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Vehicle {
+
+    String name;
+    String color;
+    double mileage;
+    double maxSpeed;
+    double fuelLevel;
+    double traveledDistance;
+    boolean running;
+
+}
