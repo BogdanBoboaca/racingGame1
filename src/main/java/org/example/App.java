8860d54d815571c3709db7f98d6acda300d88cff
BogdanBoboaca <bogdan.m.boboaca@gmail.com>
2021-12-07 14:03:22 +0200

    studied exceptions

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,8 +3,7 @@ package org.example;
 
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception {
         System.out.println( "Welcome to the Racing Game!" );
 
         Game game = new Game();
--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
@@ -11,7 +12,7 @@ public class Game {
     private List<Vehicle> competitors = new ArrayList<>();
 
 
-    public void start() {
+    public void start() throws Exception {
         System.out.println("Starting game...");
 
         initializeTracks();
@@ -41,14 +42,21 @@ public class Game {
         return scanner.nextDouble();
     }
 
-    private Track getSelectedTrackFromUser() {
+    private Track getSelectedTrackFromUser() throws Exception {
         System.out.println("Please enter Track No: ");
         Scanner scanner = new Scanner(System.in);
-        int selectedTrackNumber = scanner.nextInt();
 
-        Track selectedTrack = tracks[selectedTrackNumber - 1];
-        System.out.println("Selected track is " + selectedTrack.getName());
-        return selectedTrack;
+        try {
+            int selectedTrackNumber = scanner.nextInt();
+
+            Track selectedTrack = tracks[selectedTrackNumber - 1];
+            System.out.println("Selected track is " + selectedTrack.getName());
+            return selectedTrack;
+        } catch (InputMismatchException e) {
+            throw new RuntimeException("You have enter an invalid value");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new Exception("You have selected a non-existent track number.");
+        }
     }
 
     private void initializeCompetitors() {
